559e771d2072cab6f8078203de09c390a96cddab
Prasanna Mangoli <mangoli.prasanna05@gmail.com>
2023-01-02 09:51:03 +0530

    custom settings for toast

--- src/utils/toast.utils.js
+++ src/utils/toast.utils.js
@@ -1,7 +1,7 @@
 import toast from 'react-hot-toast';
 
 const defaultSettings = {
-  posotion: 'top-right',
+  position: 'top-right',
 }
 
 const ToastHelper = {
@@ -11,12 +11,12 @@ const ToastHelper = {
   error: (msg, settings = defaultSettings) => {
     toast.error(msg, settings);
   },
-  promise: (promise, success, error) => {
+  promise: (promise, success, error, settings = defaultSettings) => {
     toast.promise(promise, {
       loading: 'Working...',
       success,
       error,
-    });
+    }, settings);
   }
 }
 
